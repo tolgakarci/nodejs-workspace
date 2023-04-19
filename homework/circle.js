751f96bac83339702d606fc0adde11773bf73e5b
Tolga Karci <karcitolga@yahoo.com>
2023-04-19 13:08:13 +0300

    module export

---
+++ homework/circle.js
@@ -0,0 +1,9 @@
+function circleArea(number) {
+  return number * number * Math.PI
+}
+
+function circleCircumference(number) {
+  return number * Math.PI * 2
+}
+
+module.exports = { circleArea, circleCircumference }
