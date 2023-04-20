f1b6cc82db2a3305ec13cd929f5fb652699c4b70
Tolga Karci <karcitolga@yahoo.com>
2023-04-21 01:07:59 +0300

    template engine added

---
+++ PCAT/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const ejs = require('ejs')
+const path = require('path')
+
+const app = express()
+
+// template engine
+app.set('view engine', 'ejs')
+
+// middlewares
+app.use(express.static('public'))
+
+// routes
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/add', (req, res) => {
+  res.render('add')
+})
+
+const port = 3000
+
+app.listen(port, () => {
+  console.log(`Sunucu ${port} portunda başlatıldı.`)
+})
