3bd4dc7af2776b67d83c997e473b9302c171b8ff
Tolga Karci <karcitolga@yahoo.com>
2023-04-22 00:07:17 +0300

    clean blog

--- homework/cleanBlog/app.js
+++ homework/cleanBlog/app.js
@@ -1,13 +1,25 @@
 const express = require('express')
-
+const ejs = require('ejs')
 const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public'))
 
 const port = 3000
 
 const blog = { id: 1, title: 'Blog title', description: 'Blog description' }
 
 app.get('/', (req, res) => {
-  res.send(blog)
+  res.render('index')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
+app.get('/add', (req, res) => {
+  res.render('add_post')
 })
 
 app.listen(port, () => {
