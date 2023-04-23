0c814a8a4256c3c61c328860f3946cbfb32217af
Tolga Karci <karcitolga@yahoo.com>
2023-04-23 18:43:07 +0300

    MVC structure

--- PCAT/app.js
+++ PCAT/app.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const mongoose = require('mongoose')
 
+const fileUpload = require('express-fileupload')
+const methodOverride = require('method-override')
 const ejs = require('ejs')
-const path = require('path')
-const Photo = require('./models/Photo')
 
+const photoControllers = require('./controllers/photoControllers')
+const pageControllers = require('./controllers/pageControllers')
 const app = express()
 
 // connect DB
@@ -20,46 +22,23 @@ app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+app.use(fileUpload())
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+)
 
 // routes
-app.get('/', async (req, res) => {
-  // res.sendFile(path.resolve(__dirname, 'temp/index.html'))
+app.get('/', photoControllers.getAllPhotos)
+app.get('/photos/:id', photoControllers.getPhoto)
+app.post('/photos', photoControllers.createPhoto)
+app.put('/photos/:id', photoControllers.updatePhoto)
+app.delete('/photos/:id', photoControllers.deletePhoto)
 
-  const photos = await Photo.find({})
-  res.render('index', {
-    photos,
-  })
-
-  // await Photo.find({})
-  //   .exec()
-  //   .then((photos) => {
-  //     res.render('index', {
-  //       photos,
-  //     })
-  //   })
-  //   .catch((err) => {
-  //     console.log(err)
-  //   })
-})
-
-app.get('/about', (req, res) => {
-  res.render('about')
-})
-
-app.get('/add', (req, res) => {
-  res.render('add')
-})
-
-app.post('/photos', async (req, res) => {
-  await Photo.create(req.body)
-    .then(() => {
-      console.log('Item created.')
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-  res.redirect('/')
-})
+app.get('/about', pageControllers.getAboutPage)
+app.get('/add', pageControllers.getAddPage)
+app.get('/photos/edit/:id', pageControllers.getEditPage)
 
 const port = 3000
 
---
+++ PCAT/controllers/photoControllers.js
@@ -0,0 +1,60 @@
+const Photo = require('../models/Photo')
+const fs = require('fs')
+
+exports.getAllPhotos = async (req, res) => {
+  const photos = await Photo.find({}).sort('-dateCreated')
+  res.render('index', {
+    photos,
+  })
+}
+
+exports.getPhoto = async (req, res) => {
+  const id = req.params.id
+  const photo = await Photo.findById(id)
+  res.render('photo', {
+    photo,
+  })
+}
+
+exports.createPhoto = async (req, res) => {
+  const uploadDir = 'public/uploads'
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir)
+  }
+
+  uploadImage = req.files.image
+  uploadPath = __dirname + '/../public/uploads/' + uploadImage.name
+
+  uploadImage.mv(uploadPath, async () => {
+    await Photo.create({
+      ...req.body,
+      image: '/uploads/' + uploadImage.name,
+    })
+      .then(() => {
+        console.log('Item created.')
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+    res.redirect('/')
+  })
+}
+
+exports.updatePhoto = async (req, res) => {
+  const id = req.params.id
+  const photo = await Photo.findById(id)
+  photo.title = req.body.title
+  photo.description = req.body.description
+  photo.save()
+  res.redirect(`/photos/${req.params.id}`)
+}
+
+exports.deletePhoto = async (req, res) => {
+  const id = req.params.id
+  const photo = await Photo.findById(id)
+
+  const filePath = __dirname + '/../public' + photo.image
+  fs.unlinkSync(filePath)
+  await photo.deleteOne()
+  res.redirect('/')
+}
